635e06ae5bb6b20289170748d6967e9c246b79b2
Vignesh-Kamaraj <vigneshkamaraj25@gmail.com>
2023-10-16 12:02:11 +0530

    updated

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express') //commonJS Import older version
+// import express from 'express'//ES Module Import add "type": "module" in package.json
+const dotenv=require('dotenv')
+dotenv.config()
+const PORT =process.env.PORT
+const AppRoutes = require('./src/routes')
+const app = express()
+app.use(express.json())
+
+app.use('/',AppRoutes)
+
+
+app.listen(PORT,()=>console.log(`Server listening to port${PORT}`))
+
+// npm init
+// npm i express
+// "start": "node index.js",
