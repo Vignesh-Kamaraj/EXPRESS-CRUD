962472fb9437f8f06524ef36344afb8dcd28a07d
Vignesh-Kamaraj <vigneshkamaraj25@gmail.com>
2023-10-16 20:05:25 +0530

    CORS added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express') //commonJS Import older version
 // import express from 'express'//ES Module Import add "type": "module" in package.json
 const dotenv=require('dotenv')
+const cors= require('cors')
 dotenv.config()
 const PORT =process.env.PORT
 const AppRoutes = require('./src/routes')
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/',AppRoutes)
